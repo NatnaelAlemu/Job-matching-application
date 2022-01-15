5eafd193ad20d2a14467b9d4fd4197a36b5d5ca7
NatnaelAlemu <it.natnaelalemu@gmail.com>
2022-01-16 02:37:55 +0300

    add project file to github

---
+++ job-Match-Web/src/test/java/com/jobMatchWeb/JobMatchWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobMatchWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMatchWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
